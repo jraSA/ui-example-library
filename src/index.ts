87a6b05754311c5bf0ba7f86ca7174b90f5dd611
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2021-01-07 20:12:08 +0100

    added theme

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Text from './atoms/Text';
 import Card from './molecules/Card';
 import List from './organisms/List';
+import BaseTheme from './theme/BaseTheme';
+import { getThemeValue } from './theme/ThemeUtils';
 
-export { Text, Card, List };
+export { Text, Card, List, getThemeValue, BaseTheme };
--- test-build.js
+++ test-build.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
-const { Text, Card, List } = require('./lib');
+const { Text, Card, List, getThemeValue, BaseTheme } = require('./lib');
 
 assert.deepStrictEqual(typeof Text, 'function');
 assert.deepStrictEqual(typeof Card, 'function');
 assert.deepStrictEqual(typeof List, 'function');
+assert.deepStrictEqual(typeof getThemeValue, 'function');
+assert.deepStrictEqual(typeof BaseTheme, 'object');
